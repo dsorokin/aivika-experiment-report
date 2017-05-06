39e4a2d0d3cd47fbd6f206531765ab51f6b24dc6
David Sorokin <david.sorokin@gmail.com>
2017-05-06 08:33:57 +0300

    Updated tests.

---
+++ tests/MachRep3Sqlite3UsingDiagrams.hs
@@ -0,0 +1,63 @@
+
+{-# LANGUAGE FlexibleContexts #-}
+
+import Database.HDBC
+import Database.HDBC.Sqlite3
+
+import Simulation.Aivika.Experiment.Entity
+import Simulation.Aivika.Experiment.Entity.HDBC
+import Simulation.Aivika.Experiment.Report
+import Simulation.Aivika.Experiment.Report.Base
+import Simulation.Aivika.Experiment.Report.Chart
+import Simulation.Aivika.Experiment.Chart.Backend.Diagrams
+
+import Graphics.Rendering.Chart.Backend.Diagrams
+
+renderer =
+  defaultWebReportRenderer { reportParameter = DiagramsRenderer SVG loadCommonFonts }
+
+generators agent exp =
+  return
+  [reportView $ defaultExperimentSpecsView,
+   reportView $ defaultDeviationChartView {
+     deviationChartSourceKey = "deviation 1",
+     deviationChartLeftYSeries = const [],
+     deviationChartRightYSeries = filter (== "x") },
+   reportView $ defaultFinalStatsView {
+     finalStatsSourceKey = "final deviation 1",
+     finalStatsSeries = filter (== "x") },
+   reportView $ defaultTableView {
+     tableSourceKey = "time series 1",
+     tableSeries = filter (== "x") },
+   reportView $ defaultFinalHistogramView {
+     finalHistogramSourceKey = "multiple last value list 1",
+     finalHistogramSeries = filter (== "x") },
+   reportView $ defaultFinalTableView {
+     finalTableSourceKey = "last value 1" },
+   reportView $ defaultLastValueView {
+     lastValueSourceKey = "last value 1",
+     lastValueSeries = filter (== "x") },
+   reportView $ defaultTimeSeriesView {
+     timeSeriesSourceKey = "time series 1",
+     timeSeriesLeftYSeries = filter (== "t"),
+     timeSeriesRightYSeries = filter (== "x") },
+   reportView $ defaultXYChartView {
+     xyChartSourceKey = "time series 1",
+     xyChartXSeries = filter (== "t"),
+     xyChartLeftYSeries = filter (== "t"),
+     xyChartRightYSeries = filter (== "x") },
+   reportView $ defaultFinalXYChartView {
+     finalXYChartSourceKey = "last value 1",
+     finalXYChartXSeries = filter (== "n"),
+     finalXYChartLeftYSeries = filter (== "t"),
+     finalXYChartRightYSeries = filter (== "x") },
+   reportView $ defaultHistogramView {
+     histogramSourceKey = "time series 1",
+     histogramSeries = filter (== "x") }]
+
+main =
+  do conn  <- connectSqlite3 "test.db"
+     agent <- newExperimentAgent conn
+     runReportParallel agent generators renderer
+     disconnect conn
+     
